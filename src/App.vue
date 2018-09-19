9a558ed64be9004a97585491867ad5aca169f464
Cathal O'Brien <Cathal@soops.intern>
2018-09-19 11:23:39 +0200

    tut37

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+      <show-blogs></show-blogs>
+
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+
+<script>
+
+  import addBlog from './components/addBlog.vue';
+  import showBlogs from './components/showBlogs.vue';
+
+
+  export default {
+    components: {
+      'add-blog': addBlog,
+      'show-blogs': showBlogs
+    },
+
+    data() {
+      return {
+
+
+
+      }
     }
+
+
   }
-}
+</script>
+
+<style scoped lang="scss">
+  body{margin:0;
+ }
+
 </style>
---
+++ src/TutFiles/formTwo.vue
@@ -0,0 +1,89 @@
+
+Skip to content
+
+    Features
+    Platform
+    Business
+    Explore
+    Pricing
+
+Sign in
+Sign up
+
+37
+321
+
+    196
+
+iamshaunjp/vuejs-playlist
+Code
+Issues 2
+Pull requests 3
+Projects 0
+Insights
+Join GitHub today
+
+GitHub is home to over 28 million developers working together to host and review code, manage projects, and build software together.
+vuejs-playlist/src/components/formTwo.vue
+89bedc6 on Apr 3, 2017
+Shaun Pelling added lesson 28 code
+46 lines (41 sloc) 961 Bytes
+<template>
+    <div>
+        <form-helper>
+            <div slot="form-header">
+                <h3>Form Two - Log In</h3>
+                <p>Enter your details to log-in</p>
+            </div>
+            <div slot="form-fields">
+                <input type="text" placeholder="username" required />
+                <input type="password" placeholder="password" required />
+            </div>
+            <div slot="form-controls">
+                <button v-on:click="handleSubmit">Login</button>
+            </div>
+        </form-helper>
+    </div>
+</template>
+
+<script>
+// Imports
+import formHelper from './formHelper.vue'
+export default {
+    components: {
+        'form-helper': formHelper
+    },
+    data () {
+        return {
+        }
+    },
+    methods: {
+        handleSubmit: function(){
+            alert('thanks for logging in (form two)');
+        }
+    }
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
+</style>
+
+    © 2018 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Help
+
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
+Press h to open a hovercard with more details.
